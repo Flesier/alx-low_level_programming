8de8d057334c6857af6f13e58814769a7b29996b
Flesier <flesieronyango18@gmail.com>
2022-09-08 13:37:00 -0700

    size of various computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - print the various types of size on the computer
- * Description:using the main function
- * Return:Always 0
+ * main - Entry point
+ * The size of all characters..
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
+	int intType;
+	float floatType;
+	char charType;
+	long int longIntType;
+	long long int LongLongIntType;
 
-	printf("Size of char: %d byte(s)\n", sizeof(c));
-	printf("Size of int: %d byte(s)\n", sizeof(i));
-	printf("Size of long int: %d byte(s)\n", sizeof(li));
-	printf("Size of long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(LongLongIntType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
