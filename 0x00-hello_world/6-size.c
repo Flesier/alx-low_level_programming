d9513b943a3fcd97156b391b3d55899ed097189f
Flesier <flesieronyango18@gmail.com>
2022-09-08 07:15:05 -0700

    assembly language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 /**
- * main -Entry point
- *
- * Return: Always 0 (Success)
+ * main - print the various types of size on the computer
+ * Description:using the main function
+ * Return:Always 0
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char c;
+	int i;
+	long int li;
+	long long int lli;
 	float f;
-	
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	printf("size of char: %ld byte(s)\n", sizeof(c));
+	printf("size of int: %ld byte(s)\n", sizeof(i));
+	printf("size of long int: %ld byte(s)\n", sizeof(li));
+	printf("size of long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
