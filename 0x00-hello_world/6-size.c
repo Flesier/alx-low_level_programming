06d67fbce50e3538a545046370ffde70ffddc27b
Flesier <flesieronyango18@gmail.com>
2022-09-08 13:16:05 -0700

    size of various computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("size of char: %zu byte(s)\n", sizeof(c));
-	printf("size of int: %zu byte(s)\n", sizeof(i));
-	printf("size of long int: %zu byte(s)\n", sizeof(li));
-	printf("size of long long int: %zu byte(s)\n", sizeof(lli));
-	printf("size of float: %zu byte(s)\n", sizeof(f));
+	printf("Size of char: %zu byte(s)\n", sizeof(c));
+	printf("Size of int: %zu byte(s)\n", sizeof(i));
+	printf("Size of long int: %zu byte(s)\n", sizeof(li));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
